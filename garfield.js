28713eaaf6f89fce1fe3251748f30d018841026b
Edward Fish <cloudrac3r@gmail.com>
2017-12-02 20:01:05 +1300

    Allow 2-digit years and add command reference

--- garfield.js
+++ garfield.js
@@ -6,10 +6,18 @@ module.exports = function(input) {
         garfield: {
             aliases: ["garfield"],
             shortHelp: "Fetch a garfield comic strip",
-            reference: "",
+            reference: "[random] [year=*year* month=*month* date=*date*]",
             longHelp: "",
             code: function(userID, channelID, command, d) {
-                if (command.switches.year && command.switches.month && command.switches.day) {
+                if (command.switches.day) command.switches.date = command.switches.day;
+                if (command.switches.year) if (command.switches.year.length == 2) {
+                    if (parseInt(command.switches.year) > 70) {
+                        command.switches.year = "19"+command.switches.year;
+                    } else {
+                        command.switches.year = "20"+command.switches.year;
+                    }
+                }
+                if (command.switches.year && command.switches.month && command.switches.date) {
                     let date = new Date(command.switches.year+"-"+command.switches.month+"-"+command.switches.day+" 12:00 AM +0:00");
                     if (date.toUTCString() == "Invalid Date") {
                         bf.sendMessage(channelID, "That's not a valid date.", {mention: userID});
